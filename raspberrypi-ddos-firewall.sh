74b6eddbde854aca0fa9f336d1cfb8ed3c5504ad
tim0n3 <33823820+tim0n3@users.noreply.github.com>
2021-12-06 13:44:00 +0200

    Update raspberrypi-ddos-firewall.sh

--- raspberrypi-ddos-firewall.sh
+++ raspberrypi-ddos-firewall.sh
@@ -186,4 +186,165 @@ echo "Populating DPI, Allowed ports and Safe IPs"
  echo "To view all added rules/table structure:"
  echo "type iptables -t mangle -nvL --line-numbers && iptables -t raw -nvL --line-numbers && iptables -nvL --line-numbers"
  
- 
+ #! /bin/bash
+
+echo creating inbound pkt filter:
+
+iptables -t raw -A PREROUTING -p tcp --tcp-flags ALL ALL -m comment --comment "xmas pkts (xmas portscanners)" -j DROP
+iptables -t raw -A PREROUTING -p tcp --tcp-flags ALL NONE -m comment --comment "null pkts (null portscanners)" -j DROP
+iptables -t raw -A PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
+iptables -t raw -A PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
+iptables -t raw -A PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+
+echo inbound malformed pkts are being dropped at line-rate
+echo 
+echo 
+echo creating outbound pkt filter:
+
+iptables -t raw -A OUTPUT -p tcp --tcp-flags ALL ALL -m comment --comment "xmas pkts (xmas portscanners)" -j DROP
+iptables -t raw -A OUTPUT -p tcp --tcp-flags ALL NONE -m comment --comment "null pkts (null portscanners)" -j DROP
+iptables -t raw -A OUTPUT -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
+iptables -t raw -A OUTPUT -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
+iptables -t raw -A OUTPUT -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+
+echo 
+echo
+echo outbound malformed pkts are being dropped at line-rate
+echo 
+echo 
+echo creating IPv6 in/out pkt filter:
+
+ip6tables -t raw -A PREROUTING -j DROP
+
+ip6tables -t raw -A OUTPUT -j DROP
+
+echo
+echo
+echo dropping all IPv6 pkts.
+echo
+echo
+echo creating drop rules for bogons
+echo ...
+echo 
+
+iptables -t raw -A PREROUTING -s 224.0.0.0/3 -i eth0 -m comment --comment "BOGONS" -j DROP
+iptables -t raw -A PREROUTING -s 169.254.0.0/16 -i eth0 -m comment --comment "BOGONS" -j DROP
+iptables -t raw -A PREROUTING -s 172.16.0.0/12 -i eth0 -m comment --comment "BOGONS" -j DROP
+iptables -t raw -A PREROUTING -s 192.0.2.0/24 -i eth0 -m comment --comment "BOGONS" -j DROP
+iptables -t raw -A PREROUTING -s 192.168.0.0/16 -i eth0 -m comment --comment "BOGONS" -j DROP
+iptables -t raw -A PREROUTING -s 10.0.0.0/8 -i eth0 -m comment --comment "BOGONS" -j DROP
+iptables -t raw -A PREROUTING -s 0.0.0.0/8 -i eth0 -m comment --comment "BOGONS" -j DROP
+iptables -t raw -A PREROUTING -s 240.0.0.0/5 -i eth0 -m comment --comment "BOGONS" -j DROP
+iptables -t raw -A PREROUTING -s 127.0.0.0/8 ! -i lo -m comment --comment "Only lo iface can have an addr-range of 127.0.0.x/8" -j DROP
+
+echo bogons are now being dropped.
+echo
+echo
+echo RAW table has been created to drop bad pkts/traffic at line-rate (i.e. before conntrack and routing decisions)
+echo
+echo
+echo creating mangle table:
+echo ...
+echo populating PREROUTING, INPUT, FORWARD, OUTPUT and POSTROUTING rules:
+echo ...
+echo 
+echo
+
+
+
+iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
+iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -m comment --comment "DROP new packets that don't present the SYN flag" -j DROP
+iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -m comment --comment "DROP new pkts that have malformed mss values" -j DROP
+
+iptables -t mangle -A INPUT -m conntrack --ctstate INVALID -j DROP
+iptables -t mangle -A INPUT -p tcp ! --syn -m conntrack --ctstate NEW -m comment --comment "DROP new packets that don't present the SYN flag" -j DROP
+iptables -t mangle -A INPUT -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -m comment --comment "DROP new pkts that have malformed mss values" -j DROP
+
+iptables -t mangle -A FORWARD -m conntrack --ctstate INVALID -j DROP
+iptables -t mangle -A FORWARD -p tcp ! --syn -m conntrack --ctstate NEW -m comment --comment "DROP new packets that don't present the SYN flag" -j DROP
+iptables -t mangle -A FORWARD -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -m comment --comment "DROP new pkts that have malformed mss values" -j DROP
+
+iptables -t mangle -A OUTPUT -m conntrack --ctstate INVALID -j DROP
+iptables -t mangle -A OUTPUT -p tcp ! --syn -m conntrack --ctstate NEW -m comment --comment "DROP new packets that don't present the SYN flag" -j DROP
+iptables -t mangle -A OUTPUT -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -m comment --comment "DROP new pkts that have malformed mss values" -j DROP
+
+iptables -t mangle -A POSTROUTING -m conntrack --ctstate INVALID -j DROP
+iptables -t mangle -A POSTROUTING -p tcp ! --syn -m conntrack --ctstate NEW -m comment --comment "DROP new packets that don't present the SYN flag" -j DROP
+iptables -t mangle -A POSTROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -m comment --comment "DROP new pkts that have malformed mss values" -j DROP
+
+echo completed mangle table
+echo
+echo
+echo creating filter table
+echo ...
+echo populating INPUT FORWARD and OUTPUT rules:
+echo ...
+echo 
+echo
+
+
+
+
+iptables -A INPUT -i lo -m comment --comment "allow lo iface traffic"  -j ACCEPT
+iptables -A INPUT -m comment --comment "Pkt-checks" -j IN_DPI_RULES
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -m comment --comment "ESTABLISHED,RELATED conns quick accept" -j ACCEPT
+iptables -A INPUT -conntrack --ctstate NEW -s 0/0 -m comment --comment "Allowed Ports and Services for New conns" -j IN_CUSTOMRULES
+iptables -A INPUT -m comment --comment "Explicitly DROP other connections" -j DROP
+
+iptables -A FORWARD -i lo -j ACCEPT
+iptables -A FORWARD -m comment --comment "Pkt-checks" -j IN_DPI_RULES
+iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -m comment --comment "RELATED,ESTABLISHED conns quick accept" -j ACCEPT
+iptables -A FORWARD -p tcp ! --syn -m conntrack --ctstate NEW -m comment --comment "DROP new packets that don't present the SYN flag" -j DROP
+
+
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A OUTPUT -m comment --comment "Pkt-checks" -j IN_DPI_RULES
+iptables -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -m comment --comment "RELATED,ESTABLISHED conns quick accept" -j ACCEPT
+iptables -A OUTPUT -p tcp ! --syn -m conntrack --ctstate NEW -m comment --comment "DROP new packets that don't present the SYN flag" -j DROP
+iptables -A OUTPUT -m conntrack --ctstate NEW -m comment --comment "Egress - NEW counters" -j ACCEPT
+iptables -A OUTPUT -m comment --comment "Egress - DROPPED PACKETS counters" -j DROP
+
+
+iptables -A IN_DPI_RULES -m conntrack --ctstate INVALID -m comment --comment "Drop INVALID state connections" -j DROP
+iptables -A IN_DPI_RULES -p tcp ! --syn -m conntrack --ctstate NEW -m comment --comment "DROP new packets that don't present the SYN flag" -j DROP
+iptables -A IN_DPI_RULES -p ICMP --icmp-type echo-request -m comment --comment "ICMP ping" -j ACCEPT
+iptables -A IN_DPI_RULES -p ICMP --icmp-type echo-reply -m comment --comment "ICMP ping" -j ACCEPT
+iptables -A IN_DPI_RULES -p ICMP --icmp-type 11 -m comment --comment "ICMP traceroute" -j ACCEPT
+iptables -A IN_DPI_RULES -p ICMP --icmp-type PTMD -m comment --comment "PTMD" -j ACCEPT
+iptables -A IN_DPI_RULES -m comment --comment "Jump back to main filter rules" -j RETURN
+
+iptables -A IN_CUSTOMRULES -p tcp -m tcp --dport 22 -m comment --comment "Allow SSH" -j SAFEZONE
+iptables -A IN_CUSTOMRULES -p udp -m udp --dport 67 -m comment --comment "Allow dhcp" -j ACCEPT
+iptables -A IN_CUSTOMRULES -p udp -m udp --dport 68 -m comment --comment "Allow dhcp" -j ACCEPT
+iptables -A IN_CUSTOMRULES -p tcp -m tcp --dport 501 -i eth0 -m comment --comment "Allow modbus TCP" -j SAFEZONE
+iptables -A IN_CUSTOMRULES -p udp -m udp --dport 123 -m comment --comment "Allow NTP" -j ACCEPT
+iptables -A IN_CUSTOMRULES -m comment --comment "Jump back to main filter rules" -j RETURN
+iptables -A IN_CUSTOMRULES -m comment --comment "Explicit drop rule */paranoid*/" -j DROP
+
+iptables -A SAFEZONE -s 37.48.118.94/32 -m comment --comment allow-ingress-from-TIM-secure-IP -j ACCEPT
+iptables -A SAFEZONE -s 188.34.190.74/32 -m comment --comment allow-ingress-from-TIM-secure-IP -j ACCEPT
+iptables -A SAFEZONE -s 105.23.225.106/32 -m comment --comment allow-ingress-from-EDS-hq -j ACCEPT
+iptables -A SAFEZONE -s 34.90.83.14/32 -m comment --comment allow-ingress-from-EDS-hq -j ACCEPT
+iptables -A SAFEZONE -s 165.255.239.57/32 -m comment --comment allow-ingress-from-EDS-hq -j ACCEPT
+iptables -A SAFEZONE -s 35.246.178.53/32 -m comment --comment allow-ingress-from-EDS-hq -j ACCEPT
+iptables -A SAFEZONE -s 35.235.240.0/20 -m comment --comment allow-ingress-from-google-iap -j ACCEPT
+iptables -A SAFEZONE -s 10.8.0.0/24 -i tun0 -m comment --comment "Allow src 10.8.0.0/24 from tun0 iface" -j ACCEPT
+iptables -A SAFEZONE -s 192.168.0.0/16 -j ACCEPT
+iptables -A SAFEZONE -m comment --comment "JUMP back to IN_CUSTOMRULES chain" -j RETURN
+
+echo firewall configuration is complete.
+echo
+echo
+echo to view all the rules run the following script:
+echo RAW rules:
+echo "iptables -t raw -nvL --line-numbers"
+echo
+echo MANGLE rules:
+echo "iptables -t mangle -nvL --line-numbers"
+echo
+echo FILTER rules: 
+echo "iptables -t filter -nvL --line-numbers"
+echo 
+echo View all tables:
+echo
+echo "iptables -t raw -nvL --line-numbers && iptables -t mangle -nvL --line-numbers && iptables -t filter -nvL --line-numbers && iptables -t nat -nvL --line-numbers"
